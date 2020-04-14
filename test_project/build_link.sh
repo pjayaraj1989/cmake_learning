4265810309ca8ef8ea56bb70ed93ee5563f1cf6a
pjayaraj <pjayaraj@github.com>
2020-04-14 14:54:19 +0530

    Provided a lib folder for the so file generation

--- test_project/build_link.sh
+++ test_project/build_link.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
-
+curr_path=`pwd`
 compilr=gcc
 lib_name=functions
 exe=run
+lib_so_path=$curr_path/lib
 
 #create an obj file (Position ind code- PIC))
 $compilr -c -Wall -Werror -fpic $lib_name.c
 #create a shared object
-$compilr -shared -o lib$lib_name.so $lib_name.o
+$compilr -shared -o $lib_so_path/lib$lib_name.so $lib_name.o
 #link with shared lib
-$compilr -L. -Wall -o $exe main.c -l$lib_name
+$compilr -L$lib_so_path -Wall -o $exe main.c -l$lib_name
